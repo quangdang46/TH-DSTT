5bb300903be5a7638e9b3eacaf163cf02b1ca43f
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-04-06 10:26:45 +0700

    Add files via upload

---
+++ Tuan6/Ex2.py
@@ -0,0 +1,12 @@
+import numpy as np
+def infinity_norm(mt):
+    return np.max(np.sum(np.abs(mt),axis=1))
+B1=np.array([1,-7,-2,-3]).reshape(2,2)
+B2=np.array([3,6,1,0]).reshape(2,2)
+B3=np.array([5,-4, 2,-1, 2, 3,-2, 1, 0]).reshape(3,3)
+B4=np.array([3,6,-1,3,1,0,2,4,-7]).reshape(2,5)
+print(infinity_norm(B1))
+print(infinity_norm(B2))
+print(infinity_norm(B3))
+print(infinity_norm(B4))
+
