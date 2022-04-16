ad442f7859a0c90b6ef4c62010b970a8c6acc725
Trần Quang Đãng <91641923+quangdang46@users.noreply.github.com>
2022-04-17 02:14:57 +0700

    Delete Tuan7 directory

--- Tuan7/Ex3.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from math import *
-# np.set_printoptions(suppress=True)
-def orthonormal_columns(A):
-    return (np.round(A.T@A)==np.identity(A.shape[0])).all()
-y=np.array([[cos(30),-sin(30)],[sin(30),cos(30)]])
-print(orthonormal_columns(y))
